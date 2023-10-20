d2a8659e9a35a5b908150be1697aa94c54f4e6cb
dauid64 <carlosdauid64br@gmail.com>
2023-10-19 22:01:02 -0300

    Criando os relacionamentos do banco de dados

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from apps.funcionarios.models import Funcionario
 
 
 class Documento(models.Model):
     descricao = models.CharField(max_length=100)
+    dono = models.ForeignKey(Funcionario, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.descricao
