5b4da70f735ac30bb610ed65bb165b1e3291f338
dauid64 <carlosdauid64br@gmail.com>
2023-10-23 00:07:14 -0300

    Adicionando Edição, Deletar e Criação de hora extra

--- apps/registro_hora_extra/urls.py
+++ apps/registro_hora_extra/urls.py
@@ -1,11 +1,14 @@
-from .views import ( 
-    HoraExtraList
+from .views import (
+    HoraExtraList,
+    HoraExtraEdit,
+    HoraExtraDelete,
+    HoraExtraCreate
 )
 from django.urls import path
 
 urlpatterns = [
     path('', HoraExtraList.as_view(), name='list_hora_extra'),
-    # path('novo/', FuncionarioCreate.as_view(), name='create_funcionario'),
-    # path('editar/<int:pk>', FuncionarioEdit.as_view(), name='edit_funcionario'),
-    # path('deletar/<int:pk>', FuncionarioDelete.as_view(), name='delete_funcionario'),
+    path('novo/', HoraExtraCreate.as_view(), name='create_hora_extra'),
+    path('editar/<int:pk>', HoraExtraEdit.as_view(), name='edit_hora_extra'),
+    path('deletar/<int:pk>', HoraExtraDelete.as_view(), name='delete_hora_extra'),
 ]
--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -1,7 +1,8 @@
 from typing import Any
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, UpdateView, DeleteView, CreateView
 from .models import RegistroHoraExtra
-from apps.funcionarios.models import Funcionario
+from .forms import RegistroHoraExtraForm
 
 
 class HoraExtraList(ListView):
@@ -11,3 +12,28 @@ class HoraExtraList(ListView):
         empresa_logada = self.request.user.funcionario.empresa
         queryset = RegistroHoraExtra.objects.filter(funcionario__empresa=empresa_logada)
         return queryset
+
+
+class HoraExtraEdit(UpdateView):
+    model = RegistroHoraExtra
+    form_class = RegistroHoraExtraForm
+
+    def get_form_kwargs(self):
+        kwargs = super(HoraExtraEdit, self).get_form_kwargs()
+        kwargs.update({'user': self.request.user})
+        return kwargs
+
+
+class HoraExtraDelete(DeleteView):
+    model = RegistroHoraExtra
+    success_url = reverse_lazy('list_hora_extra')
+
+
+class HoraExtraCreate(CreateView):
+    model = RegistroHoraExtra
+    form_class = RegistroHoraExtraForm
+
+    def get_form_kwargs(self):
+        kwargs = super(HoraExtraCreate, self).get_form_kwargs()
+        kwargs.update({'user': self.request.user})
+        return kwargs
